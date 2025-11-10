0b793e28cea4bd7aed619b3256a9cb658d292452
Claudio Matheus <claudio1.code@gmail.com>
2025-11-10 10:45:47 -0300

    feat: implementa função atol

--- libft/srcs/stdlib/ft_atol.c
+++ libft/srcs/stdlib/ft_atol.c
@@ -6,55 +6,34 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 16:55:08 by clados-s          #+#    #+#             */
-/*   Updated: 2025/11/07 16:33:18 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/11/10 10:38:02 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	verific_overflow(unsigned long number, int sign, int digit)
+long	ft_atol(char *nbr)
 {
-	unsigned long	cutoff;
-
-	cutoff = LONG_MAX / 10;
-	if (sign == 1)
-	{
-		if (number > cutoff || (number == cutoff && digit > 7))
-			return (1);
-	}
-	else
-	{
-		if (number > cutoff || (number == cutoff && digit > 8))
-			return (1);
-	}
-	return (0);
-}
-
-long	ft_atol(char *nptr)
-{
-	int				i;
-	unsigned long	number;
-	int				sign;
-	int				digit;
+	long	result;
+	int		sign;
+	int		i;
 
 	i = 0;
-	sign = ft_signal(nptr);
-	number = 0;
-	while (ft_isspace(nptr[i]))
+	result = 0;
+	sign = 1;
+	while ((nbr[i] >= 9 && nbr[i] <= 13) || nbr[i] == 32)
 		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-		i++;
-	while (ft_isdigit(nptr[i]))
+	if (nbr[i] == '-' || nbr[i] == '+')
 	{
-		digit = nptr[i] - '0';
-		if (ft_check_overflow(number, digit, sign))
-		{
-			if (sign == 1)
-				return (LONG_MAX);
-			return (LONG_MAX * -1 - 1);
-		}
-		number = number * 10 + digit;
+		if (nbr[i] == '-')
+			sign = -1;
 		i++;
 	}
-	return ((long)number * sign);
+	while (nbr[i] >= '0' && nbr[i] <= '9')
+	{
+		result = result * 10 + (nbr[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
+
