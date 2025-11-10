7f760b8016e1b1e05c52b84e5b52fe36f6254363
Claudio Matheus <claudio1.code@gmail.com>
2025-11-10 10:44:33 -0300

    refact: utiliza a atol para impedir overflows

--- libft/srcs/stdlib/ft_atoi.c
+++ libft/srcs/stdlib/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: clados-s <clados-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 16:06:44 by clados-s          #+#    #+#             */
-/*   Updated: 2025/11/06 16:57:00 by clados-s         ###   ########.fr       */
+/*   Updated: 2025/11/10 10:41:40 by clados-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,16 @@
 
 int	ft_atoi(const char *nptr)
 {
-	int			i;
-	long int	number;
-	int			sign;
+	int		i;
+	long	number;
+	int		sign;
 
 	i = 0;
 	sign = 1;
-	number = 0;
-
-	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == ' ')
-		i++;
-	if (nptr[i] == '-' || nptr[i] == '+')
-	{
-		if (nptr[i] == '-')
-			sign *= -1;
-		i++;
-	}	
-	while (ft_isdigit(nptr[i]))
-	{
-		number = number * 10 + (nptr[i] - '0');
-		i++;
-	}
-	return ((int)number * sign);
+	number = ft_atol(nptr);
+	if (number >= INT_MAX)
+		return (INT_MAX);
+	else if (number <= INT_MIN)
+		return (INT_MIN);
+	return (number);
 }
